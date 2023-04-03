d947c47a408108ac31d5febaadbc50ecd9c753ef
Rech_Tu <872915085@qq.com>
2023-04-03 13:10:51 +0800

    110

---
+++ 110/学员管理系统_rech作业.py
@@ -0,0 +1,23 @@
+class Student:
+    """学员实体类，学员名单管理类，学员名单管理系统"""
+    pass
+
+class StudentList:
+    def __int__(self,student_list):
+        self.s_list = student_list
+
+    def get(self,student_id):
+        """根据student_id查询信息"""
+        pass
+
+if __name__ == '__main__':
+    #入参自己定义
+    s1 = Student()
+    s2 = Student()
+    s3 = Student()
+    #初始化一个成员名单
+    s_list = StudentList([s1,s2,s3])
+    #实现get()方法
+    s_list.get()
+    #实现delete
+    s_list.delete()
